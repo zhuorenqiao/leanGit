c2ca29cfb27eda4d82cbd0cd1cdc1e697f539da8
zhuoorenqiao <1042186377@qq.com>
2017-06-25 10:43:52 +0800

    initial project

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var vm = new Vue({
+	el: '#app',
+	data() {
+		return {
+			msg: 'hello world',
+			loves: [{
+				link: 'a.html',
+				love:'lanqiu'
+			},{
+				link: 'b.html',
+				love:'zuqiu'
+			},{
+				link: 'c.html',
+				love:'paiqiu'
+			}]
+		}
+	},
+	methods: {
+
+	}
+})
